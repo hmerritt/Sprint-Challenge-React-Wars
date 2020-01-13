33f72675a3ff127296728179a6eb2d192240e1b6
Harry Merritt <harry@merritt.es>
2020-01-13 03:17:16 +0000

    Get character data from api

--- starwars/src/components/Character/Character.js
+++ starwars/src/components/Character/Character.js
@@ -1,8 +1,43 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./Character.css";
 
 
-export default function Character({ character })
+export default function Character({ id })
 {
+
+    //  Set individual character attributes
+    const [character, setCharacter] = useState({
+        name:       "Loading...",
+        birth_year: "loading...",
+        gender:     "loading...",
+        height:     "loading...",
+        mass:       "loading..."
+    });
+
+
+    /*
+    *  Get individual character data
+    */
+    useEffect(() =>
+    {
+        axios.get(`https://swapi.co/api/people/${id}`)
+           .then((res) =>
+           {
+               setCharacter({
+                   name:       res.data.name,
+                   birth_year: res.data.birth_year,
+                   gender:     res.data.gender,
+                   height:     res.data.height,
+                   mass:       res.data.mass
+               });
+           })
+           .catch((err) =>
+           {
+              console.log("Failed to individual character data from swapi:", err);
+           });
+    }, []);
+
 
     return (
         <>
